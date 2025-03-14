96b1002b248a9bf6f2b4324c031a55235875ed15
Fatih Cakmak <cakmakfatih@outlook.com.tr>
2025-03-14 08:00:27 +0300

    seperated layout from page for login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,7 @@
 'use server';
 
-import CenteredLayout from '@/components/layouts/CenteredLayout';
 import LoginForm from '@/components/pages/LoginForm';
 
 export default async function Login() {
-  return (
-    <CenteredLayout>
-      <LoginForm />
-    </CenteredLayout>
-  );
+  return <LoginForm />;
 }
