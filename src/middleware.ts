511037a67a32bdfc137deac10e1574468072efc7
Fatih Cakmak <cakmakfatih@outlook.com.tr>
2025-03-15 23:43:53 +0300

    fixed redirect issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,9 @@
-import { getToken } from 'next-auth/jwt';
-import { NextRequest, NextResponse } from 'next/server';
+import { withAuth } from 'next-auth/middleware';
 
-export async function middleware(req: NextRequest) {
-  try {
-    const token = await getToken({ req, secret: process.env.JWT_SECRET });
+export default withAuth({
+  pages: {
+    signIn: '/login',
+  },
+});
 
-    if (!token || token?.error) {
-      return NextResponse.redirect(new URL('/', req.url));
-    }
-
-    const protectedPaths = ['/collections', '/edit'];
-
-    const isProtectedPath = protectedPaths.some(path => req.nextUrl.pathname.startsWith(path));
-
-    if (isProtectedPath && !token) {
-      return NextResponse.redirect(new URL('/', req.url));
-    }
-
-    return NextResponse.next();
-  } catch (_) {
-    return NextResponse.redirect(new URL('/', req.url));
-  }
-}
-
-export const config = {
-  matcher: ['/collections'],
-};
+export const config = { matcher: ['/collections(.*)'] };
